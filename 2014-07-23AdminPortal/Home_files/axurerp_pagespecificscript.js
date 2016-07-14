a83b38757c434a82af0aa185b66ebe87fc1a6bfa
Your Name <1232@132.com>
2016-07-14 17:13:16 +0800

    sff

---
+++ 2014-07-23AdminPortal/Home_files/axurerp_pagespecificscript.js
@@ -0,0 +1,18 @@
+﻿for(var i = 0; i < 13; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+document.getElementById('u0_img').tabIndex = 0;
+
+u0.style.cursor = 'pointer';
+$axure.eventManager.click('u0', function(e) {
+
+if (true) {
+
+	SetPanelVisibility('u2','toggle','fade',500);
+
+}
+});
+gv_vAlignTable['u1'] = 'center';gv_vAlignTable['u4'] = 'center';gv_vAlignTable['u5'] = 'top';gv_vAlignTable['u6'] = 'top';gv_vAlignTable['u7'] = 'top';
